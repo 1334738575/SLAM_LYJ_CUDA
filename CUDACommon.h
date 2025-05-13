7b0b88576a45f29cc3cc3206ac62c43c1d5f5cec
lyj <1334738575@qq.com>
2025-05-13 16:15:28 +0800

    update

--- CUDACommon.h
+++ CUDACommon.h
@@ -13,37 +13,45 @@ namespace SLAM_LYJ_CUDA
 	class Mat34CU
 	{
 	public:
-		Mat34CU() {
-			cudaMalloc((void**)&dataDev_, 12 * sizeof(float));
+		Mat34CU()
+		{
+			cudaMalloc((void **)&dataDev_, 12 * sizeof(float));
 		};
-		~Mat34CU() {
+		~Mat34CU()
+		{
 			cudaFree(dataDev_);
 		};
 
-		__host__ void upload(float* _data, cudaStream_t _stream=nullptr) {
-			//cudaMemcpyAsync(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice, _stream);
+		__host__ void upload(float *_data, cudaStream_t _stream = nullptr)
+		{
+			// cudaMemcpyAsync(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice, _stream);
 			cudaMemcpy(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice);
 		}
-		__host__ void uploadRotation(float* _data, cudaStream_t _stream = nullptr) {
-			//cudaMemcpyAsync(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice, _stream);
+		__host__ void uploadRotation(float *_data, cudaStream_t _stream = nullptr)
+		{
+			// cudaMemcpyAsync(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice, _stream);
 			cudaMemcpy(dataDev_, _data, 9 * sizeof(float), cudaMemcpyHostToDevice);
 		}
-		__host__ void uploadTrans(float* _data, cudaStream_t _stream = nullptr) {
-			//cudaMemcpyAsync(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice, _stream);
+		__host__ void uploadTrans(float *_data, cudaStream_t _stream = nullptr)
+		{
+			// cudaMemcpyAsync(dataDev_, _data, 12 * sizeof(float), cudaMemcpyHostToDevice, _stream);
 			cudaMemcpy(dataDev_ + 9, _data, 3 * sizeof(float), cudaMemcpyHostToDevice);
 		}
-		__host__ void download(float* _data, cudaStream_t _stream = nullptr) {
-			//cudaMemcpyAsync(_data, dataDev_, 12 * sizeof(float), cudaMemcpyDeviceToHost, _stream);
+		__host__ void download(float *_data, cudaStream_t _stream = nullptr)
+		{
+			// cudaMemcpyAsync(_data, dataDev_, 12 * sizeof(float), cudaMemcpyDeviceToHost, _stream);
 			cudaMemcpy(_data, dataDev_, 12 * sizeof(float), cudaMemcpyDeviceToHost);
 		}
-		__device__ float3 operator*(const float3& _p) const {
+		__device__ float3 operator*(const float3 &_p) const
+		{
 			float3 ret;
 			ret.x = dataDev_[0] * _p.x + dataDev_[3] * _p.y + dataDev_[6] * _p.z + dataDev_[9];
 			ret.y = dataDev_[1] * _p.x + dataDev_[4] * _p.y + dataDev_[7] * _p.z + dataDev_[10];
 			ret.z = dataDev_[2] * _p.x + dataDev_[5] * _p.y + dataDev_[8] * _p.z + dataDev_[11];
 			return ret;
 		}
-		__device__ float3 transformNormal(const float3& _n) const {
+		__device__ float3 transformNormal(const float3 &_n) const
+		{
 			float3 ret;
 			ret.x = dataDev_[0] * _n.x + dataDev_[3] * _n.y + dataDev_[6] * _n.z;
 			ret.y = dataDev_[1] * _n.x + dataDev_[4] * _n.y + dataDev_[7] * _n.z;
@@ -51,8 +59,8 @@ namespace SLAM_LYJ_CUDA
 			return ret;
 		}
 
-	//private:
-		float* dataDev_ = nullptr;
+		// private:
+		float *dataDev_ = nullptr;
 	};
 
 	/// <summary>
@@ -61,58 +69,67 @@ namespace SLAM_LYJ_CUDA
 	class CameraCU
 	{
 	public:
-		CameraCU() {
-			cudaMalloc((void**)&paramsDev_, 4 * sizeof(float));
-			cudaMalloc((void**)&paramsInvDev_, 4 * sizeof(float));
+		CameraCU()
+		{
+			cudaMalloc((void **)&paramsDev_, 4 * sizeof(float));
+			cudaMalloc((void **)&paramsInvDev_, 4 * sizeof(float));
 		};
-		~CameraCU() {
+		~CameraCU()
+		{
 			cudaFree(paramsDev_);
 			cudaFree(paramsInvDev_);
 		};
 
-		__host__ void upload(int _w, int _h, float* _params, float* _paramsInv, cudaStream_t _stream = nullptr) {
+		__host__ void upload(int _w, int _h, float *_params, float *_paramsInv, cudaStream_t _stream = nullptr)
+		{
 			w_ = _w;
 			h_ = _h;
 			cudaMemcpyAsync(paramsDev_, _params, 4 * sizeof(float), cudaMemcpyHostToDevice, _stream);
 			cudaMemcpyAsync(paramsInvDev_, _paramsInv, 4 * sizeof(float), cudaMemcpyHostToDevice, _stream);
 		}
-		__host__ void download(float* _params, float* _paramsInv, cudaStream_t _stream = nullptr) {
+		__host__ void download(float *_params, float *_paramsInv, cudaStream_t _stream = nullptr)
+		{
 			cudaMemcpyAsync(_params, paramsDev_, 4 * sizeof(float), cudaMemcpyDeviceToHost, _stream);
 			cudaMemcpyAsync(_paramsInv, paramsInvDev_, 4 * sizeof(float), cudaMemcpyDeviceToHost, _stream);
 		}
-		__device__ void pointToImage(const float3& _p3d, float3& _p2d) {
+		__device__ void pointToImage(const float3 &_p3d, float3 &_p2d)
+		{
 			float invZ = 1.0f / _p3d.z;
 			_p2d.x = _p3d.x * paramsDev_[0] * invZ + paramsDev_[2];
 			_p2d.y = _p3d.y * paramsDev_[1] * invZ + paramsDev_[3];
 			_p2d.z = _p3d.z;
 		}
-		__device__ void pointToImage(const float3& _p3d, float2& _p2d) {
+		__device__ void pointToImage(const float3 &_p3d, float2 &_p2d)
+		{
 			float invZ = 1.0f / _p3d.z;
 			_p2d.x = _p3d.x * paramsDev_[0] * invZ + paramsDev_[2];
 			_p2d.y = _p3d.y * paramsDev_[1] * invZ + paramsDev_[3];
 		}
-		__device__ void imageToPoint(const float3& _p2d, float3& _p3d) {
-			//1/fx, 0, -cx/fx,
-			//0, 1/fy, -cy/fy,
-			//0, 0, 1
+		__device__ void imageToPoint(const float3 &_p2d, float3 &_p3d)
+		{
+			// 1/fx, 0, -cx/fx,
+			// 0, 1/fy, -cy/fy,
+			// 0, 0, 1
 			_p3d.x = (_p2d.x * paramsInvDev_[0] + paramsInvDev_[2]) * _p2d.z;
 			_p3d.y = (_p2d.y * paramsInvDev_[1] + paramsInvDev_[3]) * _p2d.z;
 			_p3d.z = _p3d.z;
 		}
-		__device__ void imageToPoint(const float2& _p2d, float3& _p3d) {
+		__device__ void imageToPoint(const float2 &_p2d, float3 &_p3d)
+		{
 			_p3d.x = _p2d.x * paramsInvDev_[0] + paramsInvDev_[2];
 			_p3d.y = _p2d.y * paramsInvDev_[1] + paramsInvDev_[3];
 			_p3d.z = 1.0f;
 		}
-		__device__ void imageToPoint(const float& _u, const float& _v, float3& _p3d) {
+		__device__ void imageToPoint(const float &_u, const float &_v, float3 &_p3d)
+		{
 			_p3d.x = _u * paramsInvDev_[0] + paramsInvDev_[2];
 			_p3d.y = _v * paramsInvDev_[1] + paramsInvDev_[3];
 			_p3d.z = 1.0f;
 		}
 
-	//private:
-		float* paramsDev_ = nullptr;
-		float* paramsInvDev_ = nullptr;
+		// private:
+		float *paramsDev_ = nullptr;
+		float *paramsInvDev_ = nullptr;
 		int w_ = 0;
 		int h_ = 0;
 	};
@@ -123,33 +140,38 @@ namespace SLAM_LYJ_CUDA
 		BaseCU() {};
 		~BaseCU() {};
 
-		__device__ void add2(const float2& p1, const float2& p2, float2& p3)
+		__device__ void add2(const float2 &p1, const float2 &p2, float2 &p3)
 		{
 			p3.x = p1.x + p2.x;
 			p3.y = p1.y + p2.y;
 		}
-		__device__ void sub2(const float2& p1, const float2& p2, float2& p3) {
+		__device__ void sub2(const float2 &p1, const float2 &p2, float2 &p3)
+		{
 			p3.x = p1.x - p2.x;
 			p3.y = p1.y - p2.y;
 		}
-		__device__ float dot2(const float2& p1, const float2& p2) {
+		__device__ float dot2(const float2 &p1, const float2 &p2)
+		{
 			return p1.x * p2.x + p1.y * p2.y;
 		}
-		__device__ float crossaProduct2(const float2& p1, const float2& p2) {
+		__device__ float crossProduct2(const float2 &p1, const float2 &p2)
+		{
 			return p1.x * p2.y - p1.y * p2.x;
 		}
-		__device__ char isP2dInTriangleCU(const float2& AB, const float2& BC, const float2& CA,
-			const float2& AP, const float2& BP, const float2& CP) {
-			float v1 = crossaProduct2(AB, AP);
-			float v2 = crossaProduct2(BC, BP);
-			float v3 = crossaProduct2(CA, CP);
+		__device__ char isP2dInTriangleCU(const float2 &AB, const float2 &BC, const float2 &CA,
+										  const float2 &AP, const float2 &BP, const float2 &CP)
+		{
+			float v1 = crossProduct2(AB, AP);
+			float v2 = crossProduct2(BC, BP);
+			float v3 = crossProduct2(CA, CP);
 			if (v1 >= 0 && v2 >= 0 && v3 >= 0)
 				return (char)1;
 			if (v1 <= 0 && v2 <= 0 && v3 <= 0)
 				return (char)1;
 			return (char)0;
 		}
-		__device__ char isP2dInTriangleCU(const float2& A, const float2& B, const float2& C, const float2& P) {
+		__device__ char isP2dInTriangleCU(const float2 &A, const float2 &B, const float2 &C, const float2 &P)
+		{
 			float2 AB, BC, CA, AP, BP, CP;
 			sub2(B, A, AB);
 			sub2(C, B, BC);
@@ -160,25 +182,30 @@ namespace SLAM_LYJ_CUDA
 			return isP2dInTriangleCU(AB, BC, CA, AP, BP, CP);
 		}
 
-		__device__ void add3(const float3& p1, const float3& p2, float3& p3) {
+		__device__ void add3(const float3 &p1, const float3 &p2, float3 &p3)
+		{
 			p3.x = p1.x + p2.x;
 			p3.y = p1.y + p2.y;
 			p3.z = p1.z + p2.z;
 		}
-		__device__ void sub3(const float3& p1, const float3& p2, float3& p3) {
+		__device__ void sub3(const float3 &p1, const float3 &p2, float3 &p3)
+		{
 			p3.x = p1.x - p2.x;
 			p3.y = p1.y - p2.y;
 			p3.z = p1.z - p2.z;
 		}
-		__device__ float dot3(const float3& p1, const float3& p2) {
+		__device__ float dot3(const float3 &p1, const float3 &p2)
+		{
 			return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z;
 		}
-		__device__ void crossProduct3(const float3& p1, const float3& p2, float3& p3) {
+		__device__ void crossProduct3(const float3 &p1, const float3 &p2, float3 &p3)
+		{
 			p3.x = p1.y * p2.z - p1.z * p2.y;
 			p3.y = p1.z * p2.x - p1.x * p2.z;
 			p3.z = p1.x * p2.y - p1.y * p2.x;
 		}
-		__device__ void computePlane(const float3& A, const float3& B, const float3& C, float4& plane) {
+		__device__ void computePlane(const float3 &A, const float3 &B, const float3 &C, float4 &plane)
+		{
 			float3 AB, AC, n;
 			sub3(B, A, AB);
 			sub3(C, A, AC);
@@ -188,7 +215,8 @@ namespace SLAM_LYJ_CUDA
 			plane.z = n.z;
 			plane.w = -1 * dot3(n, A);
 		}
-		__device__ void computePlane(const float3& A, const float3& B, const float3& C, float3& abc, float& d) {
+		__device__ void computePlane(const float3 &A, const float3 &B, const float3 &C, float3 &abc, float &d)
+		{
 			float3 AB, AC;
 			sub3(B, A, AB);
 			sub3(C, A, AC);
@@ -196,24 +224,25 @@ namespace SLAM_LYJ_CUDA
 			d = -1 * dot3(abc, A);
 		}
 
-		__device__ void add4(const float4& p1, const float4& p2, float4& p3) {
+		__device__ void add4(const float4 &p1, const float4 &p2, float4 &p3)
+		{
 			p3.x = p1.x + p2.x;
 			p3.y = p1.y + p2.y;
 			p3.z = p1.z + p2.z;
 			p3.w = p1.w + p2.w;
 		}
-		__device__ void sub4(const float4& p1, const float4& p2, float4& p3) {
+		__device__ void sub4(const float4 &p1, const float4 &p2, float4 &p3)
+		{
 			p3.x = p1.x - p2.x;
 			p3.y = p1.y - p2.y;
 			p3.z = p1.z - p2.z;
 			p3.w = p1.w - p2.w;
 		}
-		__device__ float dot4(const float4& p1, const float4& p2) {
+		__device__ float dot4(const float4 &p1, const float4 &p2)
+		{
 			return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z + p1.w * p2.w;
 		}
 	};
 }
-
-
 
 #endif // !SLAM_LYJ_CUDACOMMON_H
--- test.cpp
+++ test.cpp
@@ -5,7 +5,8 @@
 #include <random>
 #include "test.h"
 
-namespace SLAM_LYJ_CUDA {
+namespace SLAM_LYJ_CUDA
+{
     CUDA_LYJ_API void test1()
     {
         int sz = 100;
@@ -13,12 +14,12 @@ namespace SLAM_LYJ_CUDA {
         std::vector<int> as(100, 0);
         std::vector<int> bs(100, 1);
         std::vector<int> cs(100, 0);
-        int* asDev;
-        int* bsDev;
-        int* csDev;
-        cudaMalloc((void**)&asDev, szBit);
-        cudaMalloc((void**)&bsDev, szBit);
-        cudaMalloc((void**)&csDev, szBit);
+        int *asDev;
+        int *bsDev;
+        int *csDev;
+        cudaMalloc((void **)&asDev, szBit);
+        cudaMalloc((void **)&bsDev, szBit);
+        cudaMalloc((void **)&csDev, szBit);
         cudaMemcpy(asDev, as.data(), szBit, cudaMemcpyHostToDevice);
         cudaMemcpy(bsDev, bs.data(), szBit, cudaMemcpyHostToDevice);
 
@@ -72,8 +73,8 @@ namespace SLAM_LYJ_CUDA {
         std::vector<float> pixels(vn * 3);
         std::vector<float> depths(w * h, FLT_MAX);
         std::vector<unsigned int> fids(w * h, UINT_MAX);
-        std::vector<int> uvs1 = { 3, 3, 3, 100, 100, 3 };
-        std::vector<int> uvs2 = { 4, 4, 4, 97, 97, 4 };
+        std::vector<int> uvs1 = {3, 3, 3, 100, 100, 3};
+        std::vector<int> uvs2 = {4, 4, 4, 97, 97, 4};
         for (int i = 0; i < fn; ++i)
         {
             int ind = 3 * i;
@@ -95,24 +96,24 @@ namespace SLAM_LYJ_CUDA {
             Pws[ind2 + 7] = (camInv[1] * uvs[5] + camInv[3]) * d;
             Pws[ind2 + 8] = d;
         }
-        auto funcCrossProduct3 = [](const float* p1, const float* p2, float* p3)
-            {
-                p3[0] = p1[1] * p2[2] - p1[2] * p2[1];
-                p3[1] = p1[2] * p2[0] - p1[0] * p2[2];
-                p3[2] = p1[0] * p2[1] - p1[1] * p2[0];
-                float n = std::sqrt(p3[0] * p3[0] + p3[1] * p3[1] + p3[2] * p3[2]);
-                p3[0] /= n;
-                p3[1] /= n;
-                p3[2] /= n;
-                // printf("%f %f %f\n", p3[0], p3[1], p3[2]);
-            };
+        auto funcCrossProduct3 = [](const float *p1, const float *p2, float *p3)
+        {
+            p3[0] = p1[1] * p2[2] - p1[2] * p2[1];
+            p3[1] = p1[2] * p2[0] - p1[0] * p2[2];
+            p3[2] = p1[0] * p2[1] - p1[1] * p2[0];
+            float n = std::sqrt(p3[0] * p3[0] + p3[1] * p3[1] + p3[2] * p3[2]);
+            p3[0] /= n;
+            p3[1] /= n;
+            p3[2] /= n;
+            // printf("%f %f %f\n", p3[0], p3[1], p3[2]);
+        };
         std::vector<float> AB(3);
         std::vector<float> AC(3);
         for (int i = 0; i < fn; ++i)
         {
-            float* A = Pws.data() + 3 * faces[3 * i];
-            float* B = Pws.data() + 3 * faces[3 * i + 1];
-            float* C = Pws.data() + 3 * faces[3 * i + 2];
+            float *A = Pws.data() + 3 * faces[3 * i];
+            float *B = Pws.data() + 3 * faces[3 * i + 1];
+            float *C = Pws.data() + 3 * faces[3 * i + 2];
             AB[0] = B[0] - A[0];
             AB[1] = B[1] - A[1];
             AB[2] = B[2] - A[2];
@@ -147,38 +148,38 @@ namespace SLAM_LYJ_CUDA {
         DepthID2 tmp;
         tmp.depth = FLT_MAX;
         tmp.fid = UINT_MAX;
-        for (auto& did : did2s)
+        for (auto &did : did2s)
             did = tmp;
         memcpy(dids.data(), did2s.data(), w * h * sizeof(uint64_t));
 
         // upload
-        float3* PwsDev;
-        cudaMalloc((void**)&PwsDev, vn * 3 * sizeof(float));
+        float3 *PwsDev;
+        cudaMalloc((void **)&PwsDev, vn * 3 * sizeof(float));
         cudaMemcpy(PwsDev, Pws.data(), vn * 3 * sizeof(float), cudaMemcpyHostToDevice);
-        float3* PcsDev;
-        cudaMalloc((void**)&PcsDev, vn * 3 * sizeof(float));
+        float3 *PcsDev;
+        cudaMalloc((void **)&PcsDev, vn * 3 * sizeof(float));
         SLAM_LYJ_CUDA::Mat34CU TDev;
         TDev.upload(Tcw.data());
-        uint3* facesDev;
-        cudaMalloc((void**)&facesDev, fn * sizeof(uint3));
+        uint3 *facesDev;
+        cudaMalloc((void **)&facesDev, fn * sizeof(uint3));
         cudaMemcpy(facesDev, faces.data(), fn * sizeof(uint3), cudaMemcpyHostToDevice);
-        float3* fNormalwsDev;
-        cudaMalloc((void**)&fNormalwsDev, fn * sizeof(float3));
+        float3 *fNormalwsDev;
+        cudaMalloc((void **)&fNormalwsDev, fn * sizeof(float3));
         cudaMemcpy(fNormalwsDev, fNormalws.data(), fn * sizeof(float3), cudaMemcpyHostToDevice);
-        float3* fNormalcsDev;
-        cudaMalloc((void**)&fNormalcsDev, fn * sizeof(float3));
-        float3* pixelsDev;
-        cudaMalloc((void**)&pixelsDev, vn * sizeof(float3));
+        float3 *fNormalcsDev;
+        cudaMalloc((void **)&fNormalcsDev, fn * sizeof(float3));
+        float3 *pixelsDev;
+        cudaMalloc((void **)&pixelsDev, vn * sizeof(float3));
         SLAM_LYJ_CUDA::CameraCU camDev;
         camDev.upload(w, h, cam.data(), camInv.data());
-        float* depthsDev;
-        cudaMalloc((void**)&depthsDev, w * h * sizeof(float));
+        float *depthsDev;
+        cudaMalloc((void **)&depthsDev, w * h * sizeof(float));
         cudaMemcpy(depthsDev, depths.data(), w * h * sizeof(float), cudaMemcpyHostToDevice);
-        unsigned int* fidsDev;
-        cudaMalloc((void**)&fidsDev, w * h * sizeof(unsigned int));
+        unsigned int *fidsDev;
+        cudaMalloc((void **)&fidsDev, w * h * sizeof(unsigned int));
         cudaMemcpy(fidsDev, fids.data(), w * h * sizeof(unsigned int), cudaMemcpyHostToDevice);
-        uint64_t* didsDev;
-        cudaMalloc((void**)&didsDev, w * h * sizeof(uint64_t));
+        uint64_t *didsDev;
+        cudaMalloc((void **)&didsDev, w * h * sizeof(uint64_t));
         cudaMemcpy(didsDev, dids.data(), w * h * sizeof(uint64_t), cudaMemcpyHostToDevice);
         SLAM_LYJ_CUDA::BaseCU baseDev;
         cudaDeviceSynchronize();
@@ -186,7 +187,7 @@ namespace SLAM_LYJ_CUDA {
         // run
         SLAM_LYJ_CUDA::ProjectorCU projector;
         projector.testTransformCUDA(TDev, PwsDev, PcsDev, vn);
-        projector.testTransformNormalCUDA(TDev, fNormalwsDev, fNormalcsDev, vn);
+        projector.testTransformNormalCUDA(TDev, fNormalwsDev, fNormalcsDev, fn);
         projector.testCameraCUDA(PcsDev, pixelsDev, vn, w, h, camDev);
         projector.testDepthAndFidCUDA(PcsDev, pixelsDev, facesDev, fNormalcsDev, fn, w, h, depthsDev, fidsDev, camDev, baseDev);
         projector.testDepthAndFidCUDA(PcsDev, pixelsDev, facesDev, fNormalcsDev, fn, w, h, didsDev, camDev, baseDev);
@@ -268,66 +269,72 @@ namespace SLAM_LYJ_CUDA {
     }
     CUDA_LYJ_API void test3()
     {
-        //base
+        // base
         int w = 160;
         int h = 120;
         std::vector<unsigned char> img(w * h * 4, 0);
-        for (int i = 0; i < h; ++i) {
-            for (int j = 0; j < w; ++j) {
+        for (int i = 0; i < h; ++i)
+        {
+            for (int j = 0; j < w; ++j)
+            {
                 int loc = (i * w + j) * 4;
-                for (int k = 0; k < 4; ++k) {
+                for (int k = 0; k < 4; ++k)
+                {
                     img[loc + k] = 255;
                 }
             }
         }
         std::vector<float> rets(w * h, 0);
-        float* retsDev;
-        
-        //malloc
-        //纹理对象
-        //texture<uchar4, cudaTextureType2D> texRef;
+        float *retsDev;
+
+        // malloc
+        // 纹理对象
+        // texture<uchar4, cudaTextureType2D> texRef;
         cudaTextureObject_t texObj;
         cudaResourceDesc resDesc;
         cudaTextureDesc texDesc;
-        //初始化资源描述符
+        // 初始化资源描述符
         memset(&resDesc, 0, sizeof(resDesc));
         resDesc.resType = cudaResourceTypeArray;
-        //初始化纹理描述符
+        // 初始化纹理描述符
         memset(&texDesc, 0, sizeof(texDesc));
         texDesc.addressMode[0] = cudaAddressModeClamp;
         texDesc.addressMode[1] = cudaAddressModeClamp;
-        if (false) {
-            texDesc.filterMode = cudaFilterModeLinear; //归一化
+        if (false)
+        {
+            texDesc.filterMode = cudaFilterModeLinear; // 归一化
             texDesc.readMode = cudaReadModeNormalizedFloat;
             texDesc.normalizedCoords = true;
         }
-        else {
-            texDesc.filterMode = cudaFilterModePoint;      // 点采样（整数坐标推荐）
-            texDesc.readMode = cudaReadModeElementType;    // 原始数据类型读取
-            texDesc.normalizedCoords = false;              // 禁用归一化坐标
+        else
+        {
+            texDesc.filterMode = cudaFilterModePoint;   // 点采样（整数坐标推荐）
+            texDesc.readMode = cudaReadModeElementType; // 原始数据类型读取
+            texDesc.normalizedCoords = false;           // 禁用归一化坐标
         }
-        //cuda数组
+        // cuda数组
         cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
         cudaArray_t cuArray;
         cudaMallocArray(&cuArray, &channelDesc, w, h);
         cudaMemcpy2DToArray(cuArray, 0, 0, img.data(), w * 4, w * 4, h, cudaMemcpyHostToDevice);
-        //数组绑定到纹理对象
+        // 数组绑定到纹理对象
         resDesc.res.array.array = cuArray;
         cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr);
-        //输出
-        cudaMalloc((void**)&retsDev, w * h * sizeof(float));
+        // 输出
+        cudaMalloc((void **)&retsDev, w * h * sizeof(float));
 
-        //process
+        // process
         testTextureCUDA(retsDev, w, h, texObj);
 
-        //output
+        // output
         cudaMemcpy(rets.data(), retsDev, w * h * sizeof(float), cudaMemcpyDeviceToHost);
-        for (int i = 0; i < w * h; ++i) {
-            //if (rets[i] != 4)
-            //    printf("111\n");
+        for (int i = 0; i < w * h; ++i)
+        {
+            // if (rets[i] != 4)
+            //     printf("111\n");
         }
-        
-        //free
+
+        // free
         cudaDestroyTextureObject(texObj);
         cudaFreeArray(cuArray);
 
